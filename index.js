e2999b43c0ca284c47eef5456e5358e8e452c89e
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-12 01:50:10 +0300

    commit tree e70b710f356532cdcc115292cbe0cfa05221d843

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "N/A"},
+]
+function superbowlWin(outcome){
+    for(let object of outcome){
+        if(object.result ==="W"){
+            return object.year
+        }
+    }
+}
+record.find(superbowlWin);
